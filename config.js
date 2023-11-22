791c5c60aeae1d4dc53dd78c24905f5bb43e9c27
fatemaranapurwala <143012337+fatemaranapurwala@users.noreply.github.com>
2023-11-22 20:02:58 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCT56MjjJhIqj4Pf0zAFYmELtKQ3ULnDak",
+  authDomain: "storytelling-app-e21ec.firebaseapp.com",
+  databaseURL: "https://storytelling-app-e21ec-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-e21ec",
+  storageBucket: "storytelling-app-e21ec.appspot.com",
+  messagingSenderId: "965564819523",
+  appId: "1:965564819523:web:eeff0ac4c71496e63a8cf6"
+};
